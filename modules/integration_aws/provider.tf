75658c643d96e845915c3ad11e108f7e5477a358
shinichi_kuboki <kuboki.business@gmail.com>
2023-03-30 01:27:12 +0900

    fix: provider

---
+++ modules/integration_aws/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  #backend "local" { path = ".tfstate" }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.74.0"
+    }
+    newrelic = {
+      source  = "newrelic/newrelic"
+      version = "3.15.0"
+    }
+  }
+}
